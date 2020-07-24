1e43261bf374f95a08bccc2d0b20caac39144c96
Brennen Tatum <bmar566@gmail.com>
2020-07-24 15:59:58 -0500

    initial commit

---
+++ MIT/Models/IndividualTranscript.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIT.Models
+{
+    public class IndividualTranscript
+    {
+    }
+}
---
+++ SQL/01_Db_Create.sql
@@ -0,0 +1,54 @@
+USE [master]
+
+IF db_id('MIT') IS NULl
+  CREATE DATABASE [MIT]
+GO
+
+USE [MIT]
+GO
+
+
+
+DROP TABLE IF EXISTS [Images];
+DROP TABLE IF EXISTS [Incident];
+DROP TABLE IF EXISTS [UserProfile];
+DROP TABLE IF EXISTS [IndividualTranscript];
+GO
+
+CREATE TABLE [UserProfile] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [FirebaseUserId] NVARCHAR(28) NOT NULL,
+  [FirstName] nvarchar(50) NOT NULL,
+  [LastName] nvarchar(50) NOT NULL,
+  [Email] nvarchar(555) NOT NULL,
+  [CreateDateTime] datetime NOT NULL,
+ 
+  CONSTRAINT UQ_FirebaseUserId UNIQUE(FirebaseUserId)
+)
+
+CREATE TABLE [Incident] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [UserProfileId] integer NOT NULL,
+  [ImageLocation] nvarchar(255),
+  [Address] nvarchar(555) NOT NULL,
+  [Comment] text NOT NULL,
+  [BeginDateTime] datetime NOT NULL,
+  [EndDateTime] datetime NOT NULL
+
+  CONSTRAINT [FK_Incident_UserProfile] FOREIGN KEY ([UserProfileId])
+	REFERENCES [UserProfile] ([Id]),
+)
+
+CREATE TABLE [IndividualTranscript] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [StartDateTime] datetime NOT NULL,
+  [Text] text NOT NULL,
+  [IncidentId] integer NOT NULL,
+  [UserProfileId] integer NOT NULL,
+
+  CONSTRAINT [FK_IndividualTranscript_Incident] FOREIGN KEY ([IncidentId]) REFERENCES [Incident] ([Id]),
+  CONSTRAINT [FK_IndividualTranscript_UserProfile] FOREIGN KEY ([UserProfileId]) REFERENCES [UserProfile] ([Id])
+)
+
+
+
