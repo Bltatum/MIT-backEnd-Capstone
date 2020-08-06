9406995e8b525ca620fa5ff8dfa86b62205a9195
Brennen Tatum <bmar566@gmail.com>
2020-08-06 11:59:17 -0500

    deleted sql files

--- SQL/SQLQuerySeedUserprofileData.sql
+++
@@ -1,4 +0,0 @@
-﻿
-set identity_insert [UserProfile] on
-insert into UserProfile (Id, FirstName, LastName, Email, CreateDateTime, FirebaseUserId) values (1, 'Brennen', 'Tatum','brennen@email.com', '2020-04-23',  'kfgfwRDBUcRfldPShk6cCRSr8rC2');
-set identity_insert [UserProfile] off
